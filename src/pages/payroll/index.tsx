6ae25c59a55e7a86c4a8223a0ca2c390084ef980
juandajedrez <117551190+juandajedrez@users.noreply.github.com>
2025-04-26 10:30:23 -0500

    prueba 1

---
+++ src/pages/payroll/index.tsx
@@ -0,0 +1,9 @@
+const Payrolls: React.FC = () => {
+    return (
+    <div>
+        
+    </div>
+    ); // El componente solo renderiza un div vacío
+  };
+  
+  export default Payrolls;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
 import { Login, Register, RegisterSupplier, UpdateSupplier, ProductCards, SupplierTable, RecipeTable, CreateRecipes } from "../pages";
+import Test from "../pages/test/Test";
+import Payroll from "../pages/payroll/payroll/index"
+import Pay from "../pages/payroll/pay/index"
+import Update from "../pages/payroll/update/index"
+import Payrolls from "../pages/payroll/index"
 
 export const router = createBrowserRouter([
   {
     path: "/", Component: App,
+  },
+  {
+    path: "test", Component: Test
   },
   {
     path: "login", Component: Login
@@ -29,8 +37,21 @@ export const router = createBrowserRouter([
   },
   {
     path: "create/recipe", Component: CreateRecipes
-  }
+  },
 
+  //rutas nomina
+  {
+    path: "payrolls", Component: Payrolls
+  },
+  {
+    path: "payrolls/payroll", Component: Payroll
+  },
+  {
+    path: "payrolls/updates", Component: Update
+  },
+  {
+    path: "payrolls/pay", Component: Pay
+  },
 
 
 
